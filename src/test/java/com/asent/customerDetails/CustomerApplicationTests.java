dc4e5f80dc9b1aea475063766c4ed499e227cfc0
ashish <gawaliashish142002@gmail.com>
2025-04-04 13:50:46 +0530

    Initial commit - Customer Management System

---
+++ src/test/java/com/asent/customerDetails/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asent.customerDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
